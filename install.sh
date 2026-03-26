7edec562c455f6b51f7e63e5c49fcb7606c52c81
arsy-01 <itn212010002@gmail.com>
2026-03-26 20:09:23 +0700

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 
+# Catatan: Jika base link download juga berubah, silakan sesuaikan URL di bawah ini.
 BASE_URL="https://github.com/arsy-01/main/releases/download/delta"
 
 install_apk() {
@@ -44,21 +45,27 @@ apk_menu() {
         echo "[2] Delta B"
         echo "[3] Delta C"
         echo "[4] Delta D"
-        echo "[5] Install Semua Sekaligus"
+        echo "[5] Delta E"
+        echo "[6] Delta F"
+        echo "[7] Install All"
         echo "[0] Kembali ke Menu Utama"
         echo "-----------------------------------"
-        read -p "Pilih APK [0-5]: " apk_choice
+        read -p "Pilih APK [0-7]: " apk_choice
 
         case $apk_choice in
             1) install_apk "Deltaa.apk" "4d92bfdcf2124b567cf29eb0b5e1eb3ba52bcc14304d1ede729fe4fcd3775378" "true" ;;
             2) install_apk "Deltab.apk" "b91f1106da9c326c07d6d906f5c88e1c8e4655ca8d5d7e14b686080529d8ba5b" "true" ;;
             3) install_apk "Deltac.apk" "2dcf91449ff5ce46a0b2ccf8379f3a978526d1e052f735207706283b33cbca65" "true" ;;
             4) install_apk "Deltad.apk" "a45ba2682b62fd75ddb00d4711d75d734d0c7dd15c43c6b97e8ccd1416a956e4" "true" ;;
-            5) 
+            5) install_apk "Deltae.apk" "8d8a4960cb8c8864b9f60f619dc6f26958c564c8dce7c58ca02f76f6418a5c6b" "true" ;;
+            6) install_apk "Deltaf.apk" "c5ce9d80a4f5ef72c6f0832d9c1990a0098c505e1421c86c15d93aa27a04bf9c" "true" ;;
+            7) 
                 install_apk "Deltaa.apk" "4d92bfdcf2124b567cf29eb0b5e1eb3ba52bcc14304d1ede729fe4fcd3775378" "false"
                 install_apk "Deltab.apk" "b91f1106da9c326c07d6d906f5c88e1c8e4655ca8d5d7e14b686080529d8ba5b" "false"
                 install_apk "Deltac.apk" "2dcf91449ff5ce46a0b2ccf8379f3a978526d1e052f735207706283b33cbca65" "false"
                 install_apk "Deltad.apk" "a45ba2682b62fd75ddb00d4711d75d734d0c7dd15c43c6b97e8ccd1416a956e4" "false"
+                install_apk "Deltae.apk" "8d8a4960cb8c8864b9f60f619dc6f26958c564c8dce7c58ca02f76f6418a5c6b" "false"
+                install_apk "Deltaf.apk" "c5ce9d80a4f5ef72c6f0832d9c1990a0098c505e1421c86c15d93aa27a04bf9c" "false"
                 echo ""
                 read -p "Semua instalasi selesai! Tekan [ENTER] untuk kembali..." dummy < /dev/tty
                 ;;

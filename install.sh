1b1f096ae2ae4fdbf2e61f564d5c474d10b0510b
arsy-01 <itn212010002@gmail.com>
2026-03-29 15:58:30 +0700

    Update install.sh

--- install.sh
+++ install.sh
@@ -56,19 +56,19 @@ apk_menu() {
         read -p "Pilih APK [0-7]: " apk_choice < /dev/tty
 
         case $apk_choice in
-            1) install_apk "Deltaa.apk" "4d92bfdcf2124b567cf29eb0b5e1eb3ba52bcc14304d1ede729fe4fcd3775378" "true" ;;
-            2) install_apk "Deltab.apk" "b91f1106da9c326c07d6d906f5c88e1c8e4655ca8d5d7e14b686080529d8ba5b" "true" ;;
-            3) install_apk "Deltac.apk" "2dcf91449ff5ce46a0b2ccf8379f3a978526d1e052f735207706283b33cbca65" "true" ;;
-            4) install_apk "Deltad.apk" "a45ba2682b62fd75ddb00d4711d75d734d0c7dd15c43c6b97e8ccd1416a956e4" "true" ;;
-            5) install_apk "Deltae.apk" "8d8a4960cb8c8864b9f60f619dc6f26958c564c8dce7c58ca02f76f6418a5c6b" "true" ;;
-            6) install_apk "Deltaf.apk" "c5ce9d80a4f5ef72c6f0832d9c1990a0098c505e1421c86c15d93aa27a04bf9c" "true" ;;
+            1) install_apk "Deltaa.apk" "0f5a04747ec49f789baa0808ce9a0fc2ea63557050e86fa83133b467d4b8f8a0" "true" ;;
+            2) install_apk "Deltab.apk" "4f2f9f67d951713b062ac517e6c2a5227b518325b596678163a3b7461147ad47" "true" ;;
+            3) install_apk "Deltac.apk" "f2ca4ed01813ee8471a3da9b374fd1785ce3d48096e0b7f808449e719711157c" "true" ;;
+            4) install_apk "Deltad.apk" "1eb5f19dfc571a181ea371ac6189d641c16152484a0638cfa12dbbe63b68032e" "true" ;;
+            5) install_apk "Deltae.apk" "327577f20548f0164ae3366fd866e487026ee137f6f83802589473ff57b61afc" "true" ;;
+            6) install_apk "Deltaf.apk" "2fb6d00c2973ee28e432fc315d6e8da45f55ac1f91fa599aa94114d50a110bf0" "true" ;;
             7) 
-                install_apk "Deltaa.apk" "4d92bfdcf2124b567cf29eb0b5e1eb3ba52bcc14304d1ede729fe4fcd3775378" "false"
-                install_apk "Deltab.apk" "b91f1106da9c326c07d6d906f5c88e1c8e4655ca8d5d7e14b686080529d8ba5b" "false"
-                install_apk "Deltac.apk" "2dcf91449ff5ce46a0b2ccf8379f3a978526d1e052f735207706283b33cbca65" "false"
-                install_apk "Deltad.apk" "a45ba2682b62fd75ddb00d4711d75d734d0c7dd15c43c6b97e8ccd1416a956e4" "false"
-                install_apk "Deltae.apk" "8d8a4960cb8c8864b9f60f619dc6f26958c564c8dce7c58ca02f76f6418a5c6b" "false"
-                install_apk "Deltaf.apk" "c5ce9d80a4f5ef72c6f0832d9c1990a0098c505e1421c86c15d93aa27a04bf9c" "false"
+                install_apk "Deltaa.apk" "0f5a04747ec49f789baa0808ce9a0fc2ea63557050e86fa83133b467d4b8f8a0" "false"
+                install_apk "Deltab.apk" "4f2f9f67d951713b062ac517e6c2a5227b518325b596678163a3b7461147ad47" "false"
+                install_apk "Deltac.apk" "f2ca4ed01813ee8471a3da9b374fd1785ce3d48096e0b7f808449e719711157c" "false"
+                install_apk "Deltad.apk" "1eb5f19dfc571a181ea371ac6189d641c16152484a0638cfa12dbbe63b68032e" "false"
+                install_apk "Deltae.apk" "327577f20548f0164ae3366fd866e487026ee137f6f83802589473ff57b61afc" "false"
+                install_apk "Deltaf.apk" "2fb6d00c2973ee28e432fc315d6e8da45f55ac1f91fa599aa94114d50a110bf0" "false"
                 echo ""
                 read -p "Semua instalasi selesai! Tekan [ENTER] untuk kembali..." dummy < /dev/tty
                 ;;
